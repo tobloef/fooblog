53e8f3c3e630b746e14664deb6872de80ceb3f10
Tobias Løfgren <tobloef@gmail.com>
2019-12-04 23:36:30 +0100

    Fix imports

--- backend/routes/user-posts/handle-get-user-posts.js
+++ backend/routes/user-posts/handle-get-user-posts.js
@@ -1,9 +1,9 @@
-import {getPosts} from "../../database/posts.js";
+import { getPostPreviews } from "../../database/posts.js";
 
 const handleGetUserPosts = async (req, res) => {
     const {username} = req.params;
     const {maxDate, limit} = req.query;
-    const posts = await getPosts(username, maxDate, limit);
+    const posts = await getPostPreviews(username, maxDate, limit);
     res.send(posts);
 };
 
